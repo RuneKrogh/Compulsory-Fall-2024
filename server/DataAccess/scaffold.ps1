bf92cda23d1fe6281dfd0d67e9f227ba5a90ddf1
Rune Krogh <runekrogh20@gmail.com>
2024-09-24 14:03:11 +0200

    lazy commit

---
+++ server/DataAccess/scaffold.ps1
@@ -0,0 +1,9 @@
+﻿dotnet ef dbcontext scaffold `
+  "Server=localhost;Database=testdb;User Id=testuser;Password=testpass;" `
+  Npgsql.EntityFrameworkCore.PostgreSQL `
+  --output-dir Models `
+  --context-dir . `
+  --context MyDbContext `
+  --no-onconfiguring `
+  --data-annotations `
+  --force
